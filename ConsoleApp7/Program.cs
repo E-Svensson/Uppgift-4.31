827827484feeafa72cea9ebdf5d8b9f5a8996619
E-Svensson <edvin.sven123@gmail.com>
2022-10-24 20:48:39 +0200

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv in ett ord: "); string Input = Console.ReadLine().ToLower();
+            Console.WriteLine();
+            char[] InputBS = Input.ToCharArray();
+            bool finns = false;
+
+            Array.Sort(InputBS);
+
+            // Går igenom bokstäverna en efter en
+            for (int i = 0; i < InputBS.Length; i++)
+            {
+
+                // Kollar om någon av de andra bokstäverna är samma bokstav
+                for (int j = 0; j < InputBS.Length; j++)
+                {
+
+                    if (j != i && InputBS[i] == InputBS[j])
+                    {
+                        if (i != 0 && InputBS[i - 1] == InputBS[j])
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            finns = true;
+                            Console.WriteLine($"Bokstaven \"{InputBS[i]}\" finns fler än en gång i ordet.");
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (finns == false)
+            {
+                Console.WriteLine("Ingen av bokstäverna i ordet finns fler än en gång.");
+            }
+        }
+    }
+}
+
